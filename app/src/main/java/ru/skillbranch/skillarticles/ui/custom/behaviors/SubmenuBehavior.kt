a620729fb1b1d8adb6855ad3f5657bfa01688ec4
pashashchyokin <pashashchyokin@gmail.com>
2020-09-17 13:27:34 +0600

    Изменения: - Реализация ViewModel. - Реализация интерфейса IArticleViewModel - Скрытие Bottombar при скролле. - Скрытие ArticleMenu при скорлле.

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
@@ -0,0 +1,33 @@
+package ru.skillbranch.skillarticles.ui.custom.behaviors
+
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.marginRight
+import ru.skillbranch.skillarticles.ui.custom.ArticleSubmenu
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
+
+class SubmenuBehavior : CoordinatorLayout.Behavior<ArticleSubmenu>() {
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
+        child: ArticleSubmenu,
+        dependency: View
+    ): Boolean {
+        return dependency is Bottombar
+    }
+
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        child: ArticleSubmenu,
+        dependency: View
+    ): Boolean {
+        return if (child.isOpen && dependency is Bottombar && dependency.translationY >= 0) {
+            animate(child, dependency)
+            true
+        } else false
+    }
+
+    private fun animate(child: View, dependency: View) {
+        val fraction = dependency.translationY / dependency.minimumHeight
+        child.translationX = (child.width + child.marginRight) * fraction
+    }
+}
